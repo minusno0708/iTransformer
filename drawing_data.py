9c8276a890029a40a920b012278946b881382777
minusno0708 <naoyao1014@gmail.com>
2024-06-16 13:42:07 +0900

    元データを描画するスクリプトを作成

--- drawing_data.py
+++ drawing_data.py
@@ -8,6 +8,8 @@ from datetime import datetime
 
 download_dir = "results/data_transitions/"
 
+divide_rate = [0.7, 0.1, 0.2]
+
 def to_datetime(date):
     return datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
 
@@ -17,17 +19,26 @@ def main(source_file):
 
     date = [to_datetime(date) for date in source[columns[0]]]
 
-    for column in columns[1:]:
+    start_len = [0, int(len(date) * divide_rate[0]), int(len(date) * (divide_rate[0] + divide_rate[1]))]
+    end_len = [start_len[1], start_len[2], len(date)]
+
+    for i, column in enumerate(columns[1:]):
         print(column)
+
+        file_name = str(i+1) + "_" + column.replace(" ", "_").replace("/", "_").replace(":", "_") + ".png"
+        values = source[column].values
 
         fig, ax = plt.subplots(figsize=(12, 5))
 
-        ax.plot(date, source[column])
+        ax.plot(date[start_len[0]:end_len[0]], values[start_len[0]:end_len[0]], label="Train", marker='o', markersize=1, linestyle='None')
+        ax.plot(date[start_len[2]:end_len[2]], values[start_len[2]:end_len[2]], label="Test", marker='o', markersize=1, linestyle='None')
+        ax.plot(date[start_len[1]:end_len[1]], values[start_len[1]:end_len[1]], label="Vali", marker='o', markersize=1, linestyle='None')
 
         plt.xlabel(columns[0])
         plt.ylabel(column)
 
-        file_name = column.replace(" ", "_").replace("/", "_").replace(":", "_") + ".png"
+        plt.legend()
+        plt.title(f"[{i+1}]{column}")
         
         plt.savefig(download_dir + file_name)
         plt.close()
